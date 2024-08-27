f186e923d37d20f1f1582427972ac651f852f913
Swapnildhanak12 <dhanakswapnil1999@gmail.com>
2024-08-27 07:48:10 +0000

    datatype

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,17 @@
+/* Datatypes
+Primitive datatypes
+Number
+String => ""
+Bigint
+Boolean => true false
+Null
+Undefined
+Symbol
+Object
+*/
+let ch= null;
+let king;
+console.log(ch);
+console.log(king);
+console.log(typeof null); // Type of null is an object
+console.log(typeof king); // Type of undefined is Undefineed
